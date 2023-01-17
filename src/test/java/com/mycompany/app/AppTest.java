72522d0c3a5177dfd0d522acbb35c77ab60e2c54
singying5230 <sing-ying.yeap@keysight.com>
2023-01-17 09:55:22 +0800

    5th change

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello world!", app.getMessage());
+        assertEquals("Hello friend!", app.getMessage());
     }
 }
